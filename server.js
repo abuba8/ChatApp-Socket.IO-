87c70571a106267732be9c84c242d3f8ae7ec944
Abdullah Ahmad <40513745+abuba8@users.noreply.github.com>
2019-04-21 15:15:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,36 @@ http.listen(3000, function() {
 app.get('/', function(req, res) {
     res.sendfile('client.html');
 });
-
-
+queue =[];
 user=-1;
 room =0;
+var roomID;
+x=1
+function client(socket,x){
+      if(x==1 || queue.length==0){
+        user++;
+        if(user%2==0){
+            room++;
+            socket.join(room)  
+            console.log("abc")
+            socket.roomID = room;
+        }
+        else{
+            socket.join(room)
+            socket.roomID = room;
+        }
+        socket.emit('connectToRoom',room)
+    }
+    if(x==0){
+        if(queue.length!=0){
+            var roomID = queue.shift();
+            socket.join(roomID)
+            console.log(roomID)
+            socket.emit('connectToRoom',roomID)
+            x=1;
+        }
+    }
+}
 
 
 //jo kaam hai wo is may hai after connection is established.
@@ -27,35 +53,34 @@ io.on('connection', function(socket){
     
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
+    client(socket,x);
     
-        
-        user++;
-        if(user%2==0){
-            room++;
-            socket.join(room)
-            
-        }
-        else{
-            socket.join(room)
-        }
-        socket.emit('connectToRoom',room)
+    
 
-   
+
 
     socket.on('disconnect',function(data){
+        roomID = socket.roomID;
+        queue.push(roomID)
+        console.log(roomID)
+        x=0;
+        console.log("hhhhhh")
+        user--;
+        //client(socket,x)
+                   
         connections.splice(connections.indexOf(socket),1);
-        
         console.log('Disconnected: %s sockets connected', connections.length);
-    });
-
+        })
     
-    socket.on('send message',function(data,room){
-        console.log(room);
-        //msg mil gya server ko, then sending back to all the connected clients in the room.
-        io.in(room).emit('new message',data,room); //agar room hata doon tou msg display hojatay hain
-        
-    })    
+       
 
-    //jani wtf?  
-});
 
+        socket.on('send message',function(data,room){
+            console.log(room);
+            //msg mil gya server ko, then sending back to all the connected clients in the room.
+            io.in(room).emit('new message',data,room); //agar room hata doon tou msg display hojatay hain
+            
+        })    
+    
+       
+    });
